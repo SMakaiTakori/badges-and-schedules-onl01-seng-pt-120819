5fe870dd973a0c6af39000548d2224c6a2d5b67d
S. Makai Takori <makait87@gmail.com>
2019-12-16 22:12:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,8 +16,6 @@ def assign_rooms(names)
 end
 
 def printer(names)
-
-batch_badge_creator(names).each{|message| puts message}
-assign_rooms(names).each{|room_assignments| puts room_assignments}
- 
+  batch_badge_creator(names).each{|message| puts message}
+  assign_rooms(names).each{|room_assignments| puts room_assignments}
 end
