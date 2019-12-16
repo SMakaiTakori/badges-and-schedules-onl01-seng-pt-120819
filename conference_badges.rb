36008face138d1a7c4f1b051143c0a847cded7aa
S. Makai Takori <makait87@gmail.com>
2019-12-16 21:01:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,11 +19,11 @@ def printer(names)
   array = []
   batch_badge_creator.each do
     puts batch_badge_creator(names)
-    batch_badge_creator
+   array.push(batch_badge_creator)
   end  
   assign_rooms.each do 
     puts assign_rooms(names)
-    assign_rooms
+    arrray.push (assign_rooms)
   end
   array(names)
 end 
